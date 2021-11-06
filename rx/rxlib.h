9c78e211ec8ff0bcb606d134affaf340044f777a
raijin0001 <raijin0001@protonmail.com>
2021-11-06 20:35:56 +0100

    update

--- rx/rxlib.h
+++ rx/rxlib.h
@@ -6,3 +6,6 @@
 bool init_rxlib(const char* seed_hash, uint32_t len, int threads, int init_threads);
 
 int64_t rx_hash_until(const char* blob, uint32_t len, uint64_t diff, int thread, char* hash_output, char* nonce_output, uint32_t* stopper); 
+int rx_remove_thread();
+int rx_add_thread();
+int seed_rxlib();
